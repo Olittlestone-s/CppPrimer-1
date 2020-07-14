cd77b1394ac4e70a1fb6a52eab8d30f4c9b395e3
Olittlestone-s <68181245+Olittlestone-s@users.noreply.github.com>
2020-07-14 15:39:37 +0800

    Update 3.34.cpp

--- Chapter3/3.34.cpp
+++ Chapter3/3.34.cpp
@@ -1,4 +1,8 @@
 /*
 计算p2与p1的“距离”，并将p1移动到p2的位置
 还不如直接写 ‘p1 = p2’ ！！！
+  Find the distance between p1 and p2 and add it to p1. Essentially, this code
+moves p1 to the position of p2. Provided that both pointers point to memory
+locations within the boundaries of the same array (or 1 past the last element)
+, the code will always be legal.
 */
