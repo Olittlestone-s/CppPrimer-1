21d4467222a1c7bf6600d1518b2e86cac284329c
Olittlestone-s <68181245+Olittlestone-s@users.noreply.github.com>
2020-07-14 10:39:15 +0800

    Update 3.21.cpp

--- Chapter3/3.21.cpp
+++ Chapter3/3.21.cpp
@@ -1,18 +1,47 @@
-#include <iostream>
-#include <vector>
-
-using std::vector;
-using std::cout;
-using std::endl;
-
+#include<iostream>
+#include<vector>
+using namespace std;
 int main(){
-    vector<int> v{1,2,3,4,5,6,7,8,9};
-    for(auto iter = v.begin(); iter != v.end(); ++iter){
-        *iter = *iter * *iter;
+    vector<int>v1;
+    vector<int>v2(10);
+    vector<int>v3(10,42);
+    vector<int>v4{10};
+    vector<int>v5{10,42};
+    vector<string>v6{10};
+    vector<string>v7{10,"hi"};
+    for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+    {
+        cout<<*it<<" ";
+        /* code */
+    }cout<<"si"<<endl;
+    for (auto it = v2.begin(); it != v2.end(); it++)
+    {
+        cout<<*it<<" ";
+        /* code */
+    }cout<<"si"<<endl;
+    for (auto it = v3.begin(); it != v3.end(); it++)
+    {
+        cout<<*it<<" ";
+        /* code */
     }
-    for(auto citer = v.cbegin(); citer != v.cend(); ++citer){
-        cout << *citer << ' ';
+    cout<<"si"<<endl;
+    for(auto it = v4.begin();it != v4.end();it++){
+        cout<<*it<<" ";
     }
-    cout << endl;
-    return 0;
+    cout<<"si"<<endl;
+    for(auto it = v5.begin();it != v5.end();it++){
+        cout<<*it<<" ";
+    }
+    cout<<"si"<<endl;
+    for (vector<string>::iterator it = v6.begin(); it != v6.end(); it++)
+    {
+        cout<<*it<<" ";
+        /* code */
+    }
+    cout<<"si"<<endl;
+    for(vector<string>::const_iterator it = v7.cbegin();it != v7.cend();it ++){
+        cout<<*it<<" ";
+    }
+    cout<<"si"<<endl;
+    return 0;   
 }
