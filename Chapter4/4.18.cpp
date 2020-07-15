6a9dc492a6f3629fe60092625e187283200fafc5
Olittlestone-s <68181245+Olittlestone-s@users.noreply.github.com>
2020-07-15 14:40:58 +0800

    Update 4.18.cpp

--- Chapter4/4.18.cpp
+++ Chapter4/4.18.cpp
@@ -1,3 +1,6 @@
 /*
 可能导致后一个字母被前一个字母的大写形式覆盖，最终导致全是同一个字母
+The first number will always be skipped. Thereafter, if a negative number is
+found, all positive numbers up to and including the first negative number will
+be printer.
 */
