96f130202ddb96fe532549809fea1bc430103b82
Olittlestone-s <68181245+Olittlestone-s@users.noreply.github.com>
2020-07-22 00:55:15 +0800

    Update 8.9.cpp

--- Chapter8/8.9.cpp
+++ Chapter8/8.9.cpp
@@ -14,3 +14,32 @@ int main(){
     cout << "从istringstream读入了：" << val << endl;
     return 0;
 }
+
+/*********************另外一种解法******************************/
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<sstream>
+using namespace std;
+struct PersonInfo{
+    string name;
+    vector<string>phones;
+};
+int main(){
+    string sline,word;
+    vector<PersonInfo> people;
+    ifstream line("people.txt");
+    while (getline(line,sline))
+    {
+        PersonInfo info;
+        std::istringstream record(sline);
+        record >> info.name;
+        while (record >> word)
+            info.phones.push_back(word);
+        people.push_back(info);
+    }
+    for(auto &elem:people){
+        cout<<elem.name<<" "<<elem.phones[0]<<endl;
+    }
+    return 0;
+}
